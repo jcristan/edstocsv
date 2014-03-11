eaec5abb7796be10607b6d2ad8c53838df2c3df2
aromerob <aromerob@redhat.com>
2014-03-11 10:45:58 -0500

    Avance Andrés Romero

--- AviancaPOCFuse/src/main/java/com/avianca/procesarASCII.java
+++
@@ -1,9 +0,0 @@
-package com.avianca;
-
-public class procesarASCII {	
-	public String doSomething(String body)
-	{		
-		body = body.replaceAll("[^\\p{L}\\p{Nd}]", ",").concat("77777\r\n");		
-		return body;
-	}
-}
